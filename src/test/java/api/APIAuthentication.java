4fb68ad054893eb3fa966187cf6ae4ac6a39934f
Duotech <duotechinstructor@gmail.com>
2022-05-17 17:51:31 -0400

    sac

---
+++ src/test/java/api/APIAuthentication.java
@@ -0,0 +1,23 @@
+package api;
+
+import org.testng.annotations.Test;
+
+public class APIAuthentication {
+
+    // What API authentication maechanisms do you know/have you used?
+
+      // I have used API key, Basic Auth, Bearer Token, JWT Token
+
+
+    @Test
+    public void basicAuth(){
+
+        //SQL,JDBC Cucumber, Jenkins CI/CD
+
+
+//        given().
+//                auth().("postman", "password").
+    }
+
+
+}
